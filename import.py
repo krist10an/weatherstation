b0db80cfc4dba3c4bdd7fce6de2ecc35581766bb
krist10an <krist10an@hotmail.com>
2011-12-19 19:25:28 +0100

    Fix typo in import

--- import.py
+++ import.py
@@ -5,7 +5,7 @@ from datalogger import *
 
 if __name__=="__main__":
 	output_file = "datalogs/weather.csv"
-	output_db = "datalogs/weather_hurra.sqlite"
+	output_db = "datalogs/weather.sqlite"
 
 	ws = WeatherStation(output_db)
 	read_csv(ws, output_file)
